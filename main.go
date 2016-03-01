2f08be8d08dcb258940732757136b3424584042b
Omer Sheikh <ojsheikh@gmail.com>
2016-03-01 12:06:12 +0500

    Remove current ranking implementation

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -10,29 +9,15 @@ import (
 )
 
 func main() {
-	var rank bool
-	flag.BoolVar(&rank, "rank", false,
-		"Rank the puzzle in addition to solving it. False by default.")
-
-	flag.Parse()
-
 	var grid sudoku.Grid
 	err := grid.Write(os.Stdin)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	ok, message := false, ""
-	if rank {
-		ok, message = grid.SolveAndRank()
-	} else {
-		ok = grid.Solve()
-	}
+	ok := grid.Solve()
 	if !ok {
 		log.Fatal("no solution exists for given sudoku puzzle")
 	}
 	fmt.Print(grid.String())
-	if message != "" {
-		fmt.Println(message)
-	}
 }
