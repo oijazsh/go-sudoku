d98aa1ed7b7dd1ea2ddffe5975b5ab72be07d297
Omer Sheikh <ojsheikh@gmail.com>
2016-04-16 15:11:01 +0500

    Do not use backticks in tests for strings.

--- bench_test.go
+++ bench_test.go
@@ -9,15 +9,15 @@ import (
 
 func BenchmarkEasy(b *testing.B) {
 	var grid sudoku.Grid
-	s := `4 _ _ _ _ _ _ 5 _
-  _ _ 3 _ _ 7 4 2 _
-  _ 5 7 8 _ _ _ _ _
-  3 4 _ 5 7 _ 2 _ 6
-  _ _ 2 4 1 9 3 _ _
-  8 _ 5 _ 6 2 _ 7 4
-  _ _ _ _ _ 4 8 6 _
-  _ 7 4 1 _ _ 9 _ _
-  _ 2 _ _ _ _ _ _ 7`
+	s := "4 _ _ _ _ _ _ 5 _\n" +
+		"_ _ 3 _ _ 7 4 2 _\n" +
+		"_ 5 7 8 _ _ _ _ _\n" +
+		"3 4 _ 5 7 _ 2 _ 6\n" +
+		"_ _ 2 4 1 9 3 _ _\n" +
+		"8 _ 5 _ 6 2 _ 7 4\n" +
+		"_ _ _ _ _ 4 8 6 _\n" +
+		"_ 7 4 1 _ _ 9 _ _\n" +
+		"_ 2 _ _ _ _ _ _ 7\n"
 	reader := strings.NewReader(s)
 	grid.Write(reader)
 
@@ -30,15 +30,15 @@ func BenchmarkEasy(b *testing.B) {
 
 func BenchmarkMedium(b *testing.B) {
 	var grid sudoku.Grid
-	s := `_ 9 2 _ 8 _ _ _ _
-  3 _ 5 _ _ _ _ _ 8
-  6 _ _ 5 _ _ 9 3 _
-  _ _ _ _ 1 _ _ 2 _
-  _ _ 9 3 2 7 6 _ _
-  _ 4 _ _ 6 _ _ _ _
-  _ 3 8 _ _ 4 _ _ 2
-  9 _ _ _ _ _ 7 _ 5
-  _ _ _ _ 7 _ 4 8 _`
+	s := "_ 9 2 _ 8 _ _ _ _\n" +
+		"3 _ 5 _ _ _ _ _ 8\n" +
+		"6 _ _ 5 _ _ 9 3 _\n" +
+		"_ _ _ _ 1 _ _ 2 _\n" +
+		"_ _ 9 3 2 7 6 _ _\n" +
+		"_ 4 _ _ 6 _ _ _ _\n" +
+		"_ 3 8 _ _ 4 _ _ 2\n" +
+		"9 _ _ _ _ _ 7 _ 5\n" +
+		"_ _ _ _ 7 _ 4 8 _\n"
 	reader := strings.NewReader(s)
 	grid.Write(reader)
 
@@ -51,15 +51,15 @@ func BenchmarkMedium(b *testing.B) {
 
 func BenchmarkHard(b *testing.B) {
 	var grid sudoku.Grid
-	s := `2 _ _ 3 7 _ 6 _ _
-  7 _ _ _ _ 5 _ 9 _
-  4 _ _ _ _ 1 _ 3 _
-  _ 9 _ _ _ _ _ 7 _
-  3 _ _ _ 4 _ _ _ 1
-  _ 6 _ _ _ _ _ 2 _
-  _ 4 _ 6 _ _ _ _ 9
-  _ 2 _ 1 _ _ _ _ 7
-  _ _ 8 _ 2 7 _ _ 4`
+	s := "2 _ _ 3 7 _ 6 _ _\n" +
+		"7 _ _ _ _ 5 _ 9 _\n" +
+		"4 _ _ _ _ 1 _ 3 _\n" +
+		"_ 9 _ _ _ _ _ 7 _\n" +
+		"3 _ _ _ 4 _ _ _ 1\n" +
+		"_ 6 _ _ _ _ _ 2 _\n" +
+		"_ 4 _ 6 _ _ _ _ 9\n" +
+		"_ 2 _ 1 _ _ _ _ 7\n" +
+		"_ _ 8 _ 2 7 _ _ 4\n"
 	reader := strings.NewReader(s)
 	grid.Write(reader)
 
@@ -72,15 +72,15 @@ func BenchmarkHard(b *testing.B) {
 
 func BenchmarkEvil(b *testing.B) {
 	var grid sudoku.Grid
-	s := `9 _ _ _ _ 8 4 7 _
-  _ 1 _ _ _ _ _ _ 6
-  _ _ 5 _ _ _ _ 2 _
-  _ _ _ 9 _ _ 2 4 1
-  _ _ _ 3 _ 4 _ _ _
-  8 4 1 _ _ 7 _ _ _
-  _ 5 _ _ _ _ 3 _ _
-  1 _ _ _ _ _ _ 5 _
-  _ 3 6 5 _ _ _ _ 9`
+	s := "9 _ _ _ _ 8 4 7 _\n" +
+		"_ 1 _ _ _ _ _ _ 6\n" +
+		"_ _ 5 _ _ _ _ 2 _\n" +
+		"_ _ _ 9 _ _ 2 4 1\n" +
+		"_ _ _ 3 _ 4 _ _ _\n" +
+		"8 4 1 _ _ 7 _ _ _\n" +
+		"_ 5 _ _ _ _ 3 _ _\n" +
+		"1 _ _ _ _ _ _ 5 _\n" +
+		"_ 3 6 5 _ _ _ _ 9\n"
 	reader := strings.NewReader(s)
 	grid.Write(reader)
 
