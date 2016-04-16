5b5f493bc5d20fa7c4f77744c484c4346d10a621
Omer Sheikh <ojsheikh@gmail.com>
2016-04-16 15:11:51 +0500

    Remove test for multiple solutions.

--- sudoku/board_test.go
+++ sudoku/board_test.go
@@ -69,22 +69,3 @@ func TestToggle(t *testing.T) {
 		t.Fail()
 	}
 }
-
-func TestRankMultipleSolution(t *testing.T) {
-	var grid Grid
-	s := `1 _ 3 _ _ 6 _ 8 _
-  _ 5 _ _ 8 _ 1 2 _
-  7 _ 9 1 _ 3 _ 5 6
-  _ 3 _ _ 6 7 _ 9 _
-  5 _ 7 8 _ _ _ 3 _
-  8 _ 1 _ 3 _ 5 _ 7
-  _ 4 _ _ 7 8 _ 1 _
-  6 _ 8 _ _ 2 _ 4 _
-  _ 1 2 _ 4 5 _ 7 8`
-
-	grid.Write(strings.NewReader(s))
-	solved, message := grid.SolveAndRank()
-	if !solved || message != rankMessage(unranked) {
-		t.Fail()
-	}
-}
